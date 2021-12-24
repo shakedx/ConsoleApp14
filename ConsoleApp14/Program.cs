7fba2ada872f89723426b8e83520b6fa1664ed0e
User <User@DESKTOP-C7DGT0E>
2021-12-24 13:50:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число:");
+            int a = int.Parse(Console.ReadLine());           
+            Reverse(a);
+            Console.ReadKey();
+        }
+
+        static void Reverse(int chislo)
+        {
+            int revchislo = chislo % 10;
+            Console.Write(revchislo);
+            chislo /= 10;
+
+            if (chislo > 0)
+                Reverse(chislo);
+        }
+    }
+}
